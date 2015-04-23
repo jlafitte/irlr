90480159aee11f9a9ba00ae688beb511ef0e6397
John Lafitte <john.lafitte@gmail.com>
2015-04-23 16:35:13 -0500

    Fixed memory leak, changed to seconds

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	"code.google.com/p/gcfg"
@@ -29,17 +30,31 @@ func main() {
 	var cfg config
 	gcfg.ReadFileInto(&cfg, "config.gcfg")
 
-	ticker := time.NewTicker(time.Second * time.Duration(cfg.Main.Interval))
-	for now := range ticker.C {
-		resp, err := http.Get(cfg.Main.URL)
-		//	var status string
+	recovered := true
 
-		if (err != nil) || (resp.StatusCode > 299 && resp.StatusCode < 400) {
-			writeit(cfg.Main.Filename, now.Format(time.RFC1123), strconv.Itoa(resp.StatusCode))
-			fmt.Println(resp.StatusCode, "at", now, " - Updating", cfg.Main.Filename, "and sleeping for", cfg.Main.Wait, "seconds")
-			time.Sleep(time.Duration(cfg.Main.Wait) * time.Second)
-		} else {
-			fmt.Println(resp.StatusCode, "at", now)
-		}
+	ticker := time.NewTicker(time.Second * time.Duration(cfg.Main.Interval))
+
+	for now := range ticker.C {
+		func() {
+			resp, err := http.Get(cfg.Main.URL)
+			defer resp.Body.Close()
+
+			if err != nil && strings.Contains(err.Error(), "redirects") {
+				if recovered {
+					recovered = false
+					writeit(cfg.Main.Filename, now.Format(time.RFC1123), strconv.Itoa(resp.StatusCode))
+					fmt.Println(resp.StatusCode, "at", now, " - Updating", cfg.Main.Filename, "and waiting for", (cfg.Main.Wait), "seconds")
+				} else {
+					fmt.Println(resp.StatusCode, "at", now, " - Not recovered from previous failure, trying again in", (cfg.Main.Wait), "seconds")
+				}
+				time.Sleep(time.Duration(cfg.Main.Wait) * time.Second)
+			} else if err != nil {
+				fmt.Println("Unknown error while trying to receive", cfg.Main.URL, ". Waiting for", (cfg.Main.Wait), "seconds")
+				time.Sleep(time.Duration(cfg.Main.Wait) * time.Second)
+			} else {
+				recovered = true
+				fmt.Println(resp.StatusCode, "at", now)
+			}
+		}()
 	}
 }
